79115e786e89ce6bd960a6e9b5673bef0e9311a1
Stavitskii <pochta100@mail.ru>
2022-08-10 17:59:31 +0300

    47:00

--- service/user.py
+++ service/user.py
@@ -1,5 +1,8 @@
-from dao.user import UserDao
+import base64
+import hashlib
 
+from dao.user import UserDao
+from constants import PWD_HASH_ITERATIONS, PWD_HASH_SALT
 
 class UserService:
     def __init__(self, dao: UserDao):
@@ -20,4 +23,13 @@ class UserService:
 
     def delete(self, uid):
         self.dao.delete(uid)
+
+    def get_hash(self, password):
+        return base64.b64encode(hashlib.pbkdf2_hmac(
+            'sha256',
+            password.encode('utf-8'),
+            PWD_HASH_SALT,
+            PWD_HASH_ITERATIONS
+        ))
+
 
