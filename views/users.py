af381ab863810b13556356f4678791f7caab8d5c
Stavitskii <pochta100@mail.ru>
2022-08-13 18:39:33 +0300

    All works

--- views/users.py
+++ views/users.py
@@ -3,17 +3,20 @@ from flask_restx import Resource, Namespace
 
 from dao.model.user import UserSchema
 from implemented import user_service
+from service.decorators import *
 
 user_ns = Namespace('users')
 
 
 @user_ns.route('/')
 class UsersView(Resource):
+    @auth_requered
     def get(self):
         rs = user_service.get_all()
         res = UserSchema(many=True).dump(rs)
         return res, 200
 
+    @admin_requered
     def post(self):
         req_json = request.json
         user = user_service.create(req_json)
@@ -22,11 +25,13 @@ class UsersView(Resource):
 
 @user_ns.route('/<int:uid>')
 class UserView(Resource):
+    @auth_requered
     def get(self, uid):
         r = user_service.get_one(uid)
         sm_d = UserSchema().dump(r)
         return sm_d, 200
 
+    @admin_requered
     def put(self, uid):
         req_json = request.json
         if "id" not in req_json:
@@ -34,6 +39,7 @@ class UserView(Resource):
         user_service.update(req_json)
         return "", 204
 
+    @admin_requered
     def delete(self, uid):
         user_service.delete(uid)
         return "", 204
